cc40a1005dd22005f4a514e61e8e051a04cc3379
jaska <pusanteemu@gmail.com>
2018-03-25 17:44:48 +0300

    Lisätty sudo

--- linux/install_dependencies.sh
+++ linux/install_dependencies.sh
@@ -1,7 +1,7 @@
-apt-get install -y curl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+sudo apt-get install -y curl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get >get_helm.sh
-chmod 700 get_helm.sh
-./get_helm.sh
+sudo curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get >get_helm.sh
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
