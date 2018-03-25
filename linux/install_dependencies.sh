a8e778b635e1c8e7bc04388782db78b2c0250f86
jaska <pusanteemu@gmail.com>
2018-03-25 17:37:30 +0300

    Lisätty install tiller.

--- linux/install_dependencies.sh
+++ linux/install_dependencies.sh
@@ -5,5 +5,3 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get >get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
-echo "Checking Tiller..."
-helm init --upgrade
