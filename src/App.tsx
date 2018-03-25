47eb5388cdc3779507b83ebe9441b05ed5492db6
jaska <pusanteemu@gmail.com>
2018-03-25 12:14:44 +0300

    Poistettu kommentteja.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { NavigationBar } from "./NavigationBar";
 import { Routes } from "./Routes";
 
-// import loader from "./spinner.gif";
-// import asd from "./logo.svg";
-
 class App extends React.Component {
 	render() {
 		return (
