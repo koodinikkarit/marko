098faa479d3562b658a77bebf44d734338560618
jaska <pusanteemu@gmail.com>
2018-03-25 16:52:33 +0300

    Muokattu install_dependencies.sh

--- linux/install_dependencies.sh
+++ linux/install_dependencies.sh
@@ -1,9 +1,4 @@
-apk add -U openssl curl tar gzip bash ca-certificates git
-wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.23-r3/glibc-2.23-r3.apk
-apk add glibc-2.23-r3.apk
-rm glibc-2.23-r3.apk
-
+apt-get install -y openssl curl tar gzip bash ca-certificates git
 curl https://kubernetes-helm.storage.googleapis.com/helm-v2.7.2-linux-amd64.tar.gz | tar zx
 mv linux-amd64/helm /usr/bin/
 helm version --client
