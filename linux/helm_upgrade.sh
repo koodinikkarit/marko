2a8699b127e97e167a558fed19ea5d621fbcc3cf
jaska <pusanteemu@gmail.com>
2018-03-27 20:02:41 +0300

    lisätty loki

--- linux/helm_upgrade.sh
+++ linux/helm_upgrade.sh
@@ -2,6 +2,8 @@ IMAGE_TAG="$CI_COMMIT_REF_NAME-$CI_COMMIT_SHA"
 
 RELEASE_NAME=$(echo "$CI_COMMIT_REF_NAME" | sed 's/\./-/g')
 
+kubectl get pods
+
 helm upgrade \
 	--wait \
 	--set markoImage=jaska/marko:$IMAGE_TAG \
